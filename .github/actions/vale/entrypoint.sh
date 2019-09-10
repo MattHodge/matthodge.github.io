7422146f5c234b5fbe43ab8c2f5790b934f578a4
Matthew Hodgkins <matthodge@gmail.com>
2019-09-10 06:28:16 +0200

    config

--- .github/actions/vale/entrypoint.sh
+++ .github/actions/vale/entrypoint.sh
@@ -5,12 +5,23 @@ IFS=$'\n\t'
 
 printenv
 
+# Uses a default config path, but allows a user to override it from a file in their repository
+configFilePath=/etc/vale/.vale.ini
+
+if [ -f "${INPUT_CONFIGFILEPATH}" ]; then
+    configFilePath="${INPUT_CONFIGFILEPATH}"
+fi
+
 echo "Settings:"
 echo ""
 echo "lintUnchangedFiles: ${INPUT_LINTUNCHANGEDFILES}"
 echo "lintDirectory: ${INPUT_LINTDIRECTORY}"
 echo "fileGlob: ${INPUT_FILEGLOB}"
+echo "configFilePath: ${configFilePath}"
 echo ""
 
 cd $GITHUB_WORKSPACE
-vale --glob="${INPUT_FILEGLOB}" "${INPUT_LINTDIRECTORY}" --output=JSON
+
+set -x
+vale --output=JSON --config="${configFilePath}" --glob="${INPUT_FILEGLOB}" "${INPUT_LINTDIRECTORY}"
+set +x
